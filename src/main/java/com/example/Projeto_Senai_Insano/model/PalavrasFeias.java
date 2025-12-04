a262aba7ac428cfc364c4ebbb120b211ddc6d95e
nicolas_camilo <nicolasflaviagabriel@gmail.com>
2025-12-04 10:12:05 -0300

    compra-shawarma deu certo

--- src/main/java/com/example/Projeto_Senai_Insano/model/PalavrasFeias.java
+++ src/main/java/com/example/Projeto_Senai_Insano/model/PalavrasFeias.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name= "palavras_feias")
 public class PalavrasFeias {
+
+    public PalavrasFeias() {
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,5 +19,19 @@ public class PalavrasFeias {
 
     private String feia;
 
-    // getters e setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFeia() {
+        return feia;
+    }
+
+    public void setFeia(String feia) {
+        this.feia = feia;
+    }
 }
